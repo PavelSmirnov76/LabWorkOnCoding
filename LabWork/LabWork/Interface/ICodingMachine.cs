338bfe2ad37334f2d4607b40d69ed66a031d8a9b
Смирнов Павел <cocacola76rus@outlook.com>
2020-05-01 12:57:36 +0300

    Добавлен интерфейс, ICodingMachine который является шаблоном для всех кодирующих алгоритмов.

---
+++ LabWork/LabWork/Interface/ICodingMachine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork.Interface
+{
+    /// <summary>
+    /// Интерфейс, который является шаблоном для всех кодирующих алгоритмов.
+    /// </summary>
+    internal interface ICodingMachine
+    {
+        /// <summary>
+        /// Метод, который сжимает текст с помощью кодирующих алгоритмов.
+        /// </summary>
+        /// <param name="Text"> Текст который надо сжать </param>
+        /// <returns> Сжатый текст </returns>
+        internal string CompressionText(string Text);
+        /// <summary>
+        /// Метод, который считает степень сжатия текста.
+        /// </summary>
+        /// <param name="Text"> Текст который надо сжать </param>
+        /// /// <param name="СompressedText"> Сжатый текст </param>
+        /// <returns> Степень сжатия текста </returns>
+        internal string CompressionRatio(string Text, string СompressedText);
+        /// <summary>
+        /// Метод, который декодирует сжатый текст.
+        /// </summary>
+        /// /// <param name="СompressedText"> Текст который надо раскодировать </param>
+        /// <returns> Раскодированный текст </returns>
+        internal string Decoder(string СompressedText);
+
+
+
+    }
+}
