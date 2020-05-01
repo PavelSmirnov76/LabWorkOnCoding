099c7bde78f36ad5dfe111b7be5e83e3ed5e0ab6
Смирнов Павел <cocacola76rus@outlook.com>
2020-05-01 12:31:36 +0300

    Добавлен пустой проект.

---
+++ LabWork/LabWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
