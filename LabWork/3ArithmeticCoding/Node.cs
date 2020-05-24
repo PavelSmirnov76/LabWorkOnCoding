b414b9441abc8418d97a09548327a487669acbb8
Смирнов Павел <cocacola76rus@outlook.com>
2020-05-24 03:54:22 +0300

    Добавли проект для лабы 3.

---
+++ LabWork/3ArithmeticCoding/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3ArithmeticCoding
+{
+    public class Node
+    {
+        public char Symbol { get; set; }
+        public double Low { get; set; }
+        public double High { get; set; }
+        public double Range { get; set; }
+        public int Frequency { get; set; }
+    }
+}
